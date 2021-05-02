c7a4e328443a3ebedc62a329aae280a8ea51cd3d
m00tt <andreamottini15@gmail.com>
2021-05-02 15:34:24 +0200

    Redefined imageview as imageButton. Created onClick methon to change textview values. Set new strings resources.

--- app/src/main/java/com/example/cookingapp/ui/converter/ConverterFragment.kt
+++ app/src/main/java/com/example/cookingapp/ui/converter/ConverterFragment.kt
@@ -1,6 +1,9 @@
 package com.example.cookingapp.ui.converter
 
+import android.content.res.loader.ResourcesLoader
+import android.graphics.PointF.length
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +12,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.cookingapp.R
+import android.widget.Toast
+import com.example.cookingapp.MainActivity
 import com.example.cookingapp.ui.converter.ConverterViewModel
+import kotlinx.android.synthetic.main.fragment_converter.*
 
 class ConverterFragment : Fragment() {
 
@@ -29,4 +35,24 @@ class ConverterFragment : Fragment() {
         })
         return root
     }
+
+    override fun onStart() {
+        super.onStart()
+        converter_change_values.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val myResources = getResources()  //Verifica della resource table
+                if (converter_tv_gr.text.equals(myResources.getText(R.string.converter_gr_text_title))){
+                    converter_tv_gr.text = myResources.getText(R.string.converter_grassiveg_text_title)
+                    converter_tv_spoon.text = myResources.getText(R.string.converter_grassianm_text_title)
+                }
+                else{
+                    converter_tv_gr.text = myResources.getText(R.string.converter_gr_text_title)
+                    converter_tv_spoon.text = myResources.getText(R.string.converter_spoon_text_title)
+                }
+            }
+        })
+    }
+
+
+
 }
