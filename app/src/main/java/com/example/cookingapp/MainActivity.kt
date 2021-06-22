37335e1c8aa4da0555244232d188e41ab7b4e0b3
m00tt <andreamottini15@gmail.com>
2021-06-22 21:55:08 +0200

    Create funzioni di recupero dati Shoplist Create nuove risorse Creato intent per condivisione Effettuati controlli inserimento

--- app/src/main/java/com/example/cookingapp/MainActivity.kt
+++ app/src/main/java/com/example/cookingapp/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.example.cookingapp
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.cookingapp.ui.cookbook.CookbookFragment
-import com.example.cookingapp.ui.home.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_shoplist.*
+import kotlinx.android.synthetic.main.fragment_shoplist.view.*
+import kotlinx.android.synthetic.main.row_shoplist.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,6 +39,52 @@ class MainActivity : AppCompatActivity() {
     fun onDelete(v: View) {
         //if(parent_lineal_layout.childCount > 2)
             parent_lineal_layout.removeView(v.parent as View)
+    }
+
+    @SuppressLint("ResourceType")
+    fun getShoplistItems(v:View) {
+        var go = false
+        var ret_str = ""
+        val item_count = parent_lineal_layout.childCount - 2
+        var index = 0
+        if (item_count >= 1) {
+            ret_str = ""
+            for (item in 0 until item_count) {
+                if (parent_lineal_layout.getChildAt(index).et_shoplist_product != null) {
+                    if (parent_lineal_layout[index].et_shoplist_product.text.isEmpty() || parent_lineal_layout[index].et_shoplist_qty.text.isEmpty()) {
+                        Toast.makeText(this, resources.getString(R.string.shoplist_compiler_check), Toast.LENGTH_SHORT).show()
+                        go = false
+                        break
+                    }else{
+                        ret_str += parent_lineal_layout[index].et_shoplist_qty.text
+                        if(resources.getStringArray(R.array.shoplist_units)[6].toString() != parent_lineal_layout[index].spin_shoplist_units.selectedItem.toString()) {
+                                ret_str += parent_lineal_layout[index].spin_shoplist_units.selectedItem.toString()
+                                ret_str += " ${resources.getString(R.string.shoplist_separator_preposition)}"
+                        }
+                        ret_str += " ${parent_lineal_layout[index].et_shoplist_product.text} \n"
+                        index += 1
+                        go = true
+                    }
+                }
+            }
+
+            Toast.makeText(this, ret_str, Toast.LENGTH_SHORT).show()
+            if(go) {
+                val sendIntent = Intent()
+                sendIntent.action = Intent.ACTION_SEND
+                sendIntent.putExtra(Intent.EXTRA_TEXT, ret_str)
+                sendIntent.type = "text/plain"
+                Intent.createChooser(sendIntent, "Share via")
+                startActivity(sendIntent)
+            }
+
+        }else {
+            Toast.makeText(
+                this,
+                resources.getString(R.string.shopllist_fab_hint),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
 }
--- app/src/main/java/com/example/cookingapp/ui/shoplist/ShoplistFragment.kt
+++ app/src/main/java/com/example/cookingapp/ui/shoplist/ShoplistFragment.kt
@@ -1,19 +1,15 @@
 package com.example.cookingapp.ui.shoplist
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.PopupMenu
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.example.cookingapp.MainActivity
 import com.example.cookingapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_shoplist.*
-import java.util.zip.Inflater
 
 
 class ShoplistFragment : Fragment() {
@@ -26,16 +22,6 @@ class ShoplistFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_shoplist, container, false)
     }
-
-
-
-
-
-
-
-
-
-
 
 
 }
