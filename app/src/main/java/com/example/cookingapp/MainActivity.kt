33562bc8ee8b2fa38b4e910cf62be1540fdab58d
m00tt <andreamottini15@gmail.com>
2021-06-25 11:53:43 +0200

    Creato restore lista della spesa. Gestita parte landscape/potrait

--- app/src/main/java/com/example/cookingapp/MainActivity.kt
+++ app/src/main/java/com/example/cookingapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.cookingapp
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -29,61 +31,10 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun onAddField(v: View) {
-        val inflater = this.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val rowView: View = inflater.inflate(R.layout.row_shoplist, container, false)
-        // Add the new row before the add field button.
-        parent_lineal_layout.addView(rowView, parent_lineal_layout.childCount - 2)
-    }
-
     fun onDelete(v: View) {
-        //if(parent_lineal_layout.childCount > 2)
-            parent_lineal_layout.removeView(v.parent as View)
+        parent_lineal_layout.removeView(v.parent as View)
     }
 
-    @SuppressLint("ResourceType")
-    fun getShoplistItems(v:View) {
-        var go = false
-        var ret_str = ""
-        val item_count = parent_lineal_layout.childCount - 2
-        var index = 0
-        if (item_count >= 1) {
-            ret_str = ""
-            for (item in 0 until item_count) {
-                if (parent_lineal_layout.getChildAt(index).et_shoplist_product != null) {
-                    if (parent_lineal_layout[index].et_shoplist_product.text.isEmpty() || parent_lineal_layout[index].et_shoplist_qty.text.isEmpty()) {
-                        Toast.makeText(this, resources.getString(R.string.shoplist_compiler_check), Toast.LENGTH_SHORT).show()
-                        go = false
-                        break
-                    }else{
-                        ret_str += parent_lineal_layout[index].et_shoplist_qty.text
-                        if(resources.getStringArray(R.array.shoplist_units)[6].toString() != parent_lineal_layout[index].spin_shoplist_units.selectedItem.toString()) {
-                                ret_str += parent_lineal_layout[index].spin_shoplist_units.selectedItem.toString()
-                                ret_str += " ${resources.getString(R.string.shoplist_separator_preposition)}"
-                        }
-                        ret_str += " ${parent_lineal_layout[index].et_shoplist_product.text} \n"
-                        index += 1
-                        go = true
-                    }
-                }
-            }
 
-            if(go) {
-                val sendIntent = Intent()
-                sendIntent.action = Intent.ACTION_SEND
-                sendIntent.putExtra(Intent.EXTRA_TEXT, ret_str)
-                sendIntent.type = "text/plain"
-                Intent.createChooser(sendIntent, "Share via")
-                startActivity(sendIntent)
-            }
-
-        }else {
-            Toast.makeText(
-                this,
-                resources.getString(R.string.shopllist_fab_hint),
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
 
 }
--- app/src/main/java/com/example/cookingapp/ui/shoplist/ShoplistFragment.kt
+++ app/src/main/java/com/example/cookingapp/ui/shoplist/ShoplistFragment.kt
@@ -1,15 +1,26 @@
 package com.example.cookingapp.ui.shoplist
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.example.cookingapp.MainActivity
 import com.example.cookingapp.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_converter.*
 import kotlinx.android.synthetic.main.fragment_shoplist.*
+import kotlinx.android.synthetic.main.row_shoplist.view.*
 
 
 class ShoplistFragment : Fragment() {
@@ -24,4 +35,108 @@ class ShoplistFragment : Fragment() {
     }
 
 
+
+    override fun onStart() {
+        super.onStart()
+
+        shoplist_fab_share.setOnClickListener {
+            var go = false
+            var retstr = ""
+            val itemcount = parent_lineal_layout.childCount - 2
+            var index = 0
+            if (itemcount >= 1) {
+                retstr = ""
+                for (item in 0 until itemcount) {
+                    if (parent_lineal_layout.getChildAt(index).et_shoplist_product != null) {
+                        if (parent_lineal_layout[index].et_shoplist_product.text.isEmpty() || parent_lineal_layout[index].et_shoplist_qty.text.isEmpty()) {
+                            Toast.makeText(
+                                context as MainActivity,
+                                resources.getString(R.string.shoplist_compiler_check),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            go = false
+                            break
+                        } else {
+                            retstr += parent_lineal_layout[index].et_shoplist_qty.text
+                            if (resources.getStringArray(R.array.shoplist_units)[6].toString() != parent_lineal_layout[index].spin_shoplist_units.selectedItem.toString()) {
+                                retstr += parent_lineal_layout[index].spin_shoplist_units.selectedItem.toString()
+                                retstr += " ${resources.getString(R.string.shoplist_separator_preposition)}"
+                            }
+                            retstr += " ${parent_lineal_layout[index].et_shoplist_product.text} \n"
+                            index += 1
+                            go = true
+                        }
+                    }
+                }
+
+                if (go) {
+                    val sendIntent = Intent()
+                    sendIntent.action = Intent.ACTION_SEND
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, retstr)
+                    sendIntent.type = "text/plain"
+                    Intent.createChooser(sendIntent, "Share via")
+                    startActivity(sendIntent)
+                }
+
+            } else {
+                Toast.makeText(
+                    context as MainActivity,
+                    resources.getString(R.string.shopllist_fab_hint),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        btn_shoplist_add.setOnClickListener {
+            onAddField()
+        }
+
+    }
+
+    //salvataggio dello stato in modo da passare valori tra Portrait e Landscape
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        var childCountParent = 0
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            outState.putInt("childParentCount", parent_lineal_layout.childCount)
+            childCountParent = parent_lineal_layout.childCount
+        }
+        else{
+            outState.putInt("childParentCount", parent_lineal_layout.childCount -2)
+            childCountParent = parent_lineal_layout.childCount -2
+        }
+        for((index) in (0 until childCountParent).withIndex()){
+            outState.putString("product$index", parent_lineal_layout[index].et_shoplist_product.text.toString())
+            outState.putString("value$index", parent_lineal_layout[index].et_shoplist_qty.text.toString())
+            outState.putInt("units$index", parent_lineal_layout[index].spin_shoplist_units.selectedItemPosition)
+        }
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        //Se l'istanza salvata non è null, allora ripristino i valori salvati
+        if(savedInstanceState != null){
+            for((index) in (0 until savedInstanceState.getInt("childParentCount")).withIndex()){
+                onAddField()
+                parent_lineal_layout[index].et_shoplist_product.setText(savedInstanceState.getString("product$index"))
+                parent_lineal_layout[index].et_shoplist_qty.setText(savedInstanceState.getString("value$index"))
+                parent_lineal_layout[index].spin_shoplist_units.setSelection(savedInstanceState.getInt("units$index"))
+            }
+
+        }
+    }
+
+    fun onAddField() {
+        val cnt = context as MainActivity
+        val inflater = cnt.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val toInflate:View = inflater.inflate(R.layout.row_shoplist, parent_lineal_layout, false)
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            parent_lineal_layout.addView(toInflate, parent_lineal_layout.childCount)
+        }
+        else{
+            parent_lineal_layout.addView(toInflate, parent_lineal_layout.childCount - 2)
+        }
+    }
 }
